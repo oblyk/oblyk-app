aed5a2ea69dba58a2a7c588f5d781aac924afe62
clucien <chastanlucien@gmail.com>
2021-01-07 19:29:22 +0100

    Ajout d'une box pour activer la recherche de paretnaire

--- src/components/users/Dashboard.vue
+++ src/components/users/Dashboard.vue
@@ -5,6 +5,12 @@
     <div v-else>
       <user-head :user="meUser" />
       <user-tabs :user="meUser" />
+      <v-container class="dashboard-container">
+        <enable-partner-search
+          v-if="meUser.partner_search === null"
+          :user="meUser"
+        />
+      </v-container>
     </div>
   </div>
 </template>
@@ -14,9 +20,16 @@ import Spinner from '@/components/layouts/Spiner'
 import { MeUserConcern } from '@/concerns/MeUserConcern'
 import UserHead from '@/components/users/layouts/UserHead'
 import UserTabs from '@/components/users/layouts/UserTabs'
+import EnablePartnerSearch from '@/components/users/EnablePartnerSearch'
 export default {
   name: 'Dashboard',
   mixins: [MeUserConcern],
-  components: { UserTabs, UserHead, Spinner }
+  components: { EnablePartnerSearch, UserTabs, UserHead, Spinner }
 }
 </script>
+
+<style lang="scss" scoped>
+.dashboard-container {
+  max-width: 900px;
+}
+</style>
---
+++ src/components/users/EnablePartnerSearch.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-card v-if="!IHaveAnAnswer">
+    <div class="d-flex flex-no-wrap justify-start">
+      <v-avatar
+        class="ma-3"
+        size="170"
+        width="170"
+        tile
+      >
+        <v-img contain :src="require('@/assets/svgs/enable-partner-search.svg')"></v-img>
+      </v-avatar>
+      <div class="flex-grow-1">
+        <v-card-title>
+          {{ $t('components.user.bePartPartner') }}
+        </v-card-title>
+
+        <v-card-text>
+          {{ $t('components.user.bePartPartnerExplain') }}
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            right
+            text
+            color="primary"
+            @click="no()"
+          >
+            {{ $t('actions.no') }}
+          </v-btn>
+          <v-btn
+            right
+            text
+            color="primary"
+            @click="yes()"
+          >
+            {{ $t('actions.yes') }}
+          </v-btn>
+        </v-card-actions>
+      </div>
+    </div>
+    <overlay-form :overlay="answerSending" />
+  </v-card>
+</template>
+<script>
+import UserApi from '@/services/oblyk-api/userApi'
+import OverlayForm from '@/components/forms/OverlayForm'
+
+export default {
+  name: 'EnablePartnerSearch',
+  components: { OverlayForm },
+  props: {
+    user: Object
+  },
+
+  data () {
+    return {
+      IHaveAnAnswer: false,
+      answerSending: false
+    }
+  },
+
+  methods: {
+    no: function () {
+      this.answerSending = true
+      UserApi
+        .update({ partner_search: false })
+        .then(() => {
+          this.IHaveAnAnswer = true
+        })
+        .catch(err => {
+          this.$root.$emit('alertFromApiError', err, 'user')
+        })
+        .finally(() => {
+          this.answerSending = false
+        })
+    },
+
+    yes: function () {
+      this.answerSending = true
+      UserApi
+        .update({ partner_search: true })
+        .then(() => {
+          this.$router.push(this.user.meUrl('partner-settings'))
+        })
+        .catch(err => {
+          this.$root.$emit('alertFromApiError', err, 'user')
+        })
+        .finally(() => {
+          this.answerSending = false
+        })
+    }
+  }
+}
+</script>
