bc6c25155e9b91825360339eb6efad316af44af0
clucien <chastanlucien@gmail.com>
2021-01-09 19:15:47 +0100

    Externalisation des onglets d'une falaise

---
+++ src/components/crags/layouts/CragTabs.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-tabs
+    show-arrows
+  >
+    <v-tab to="infos">
+      {{ $t('components.crag.tabs.info') }}
+    </v-tab>
+    <v-tab to="routes">
+      <v-badge
+        color="primary"
+        inline
+        :content="crag.routes_figures.route_count"
+      >
+        {{ $t('components.crag.tabs.routes') }}
+      </v-badge>
+    </v-tab>
+    <v-tab to="photos">
+      <v-badge
+        color="primary"
+        inline
+        :content="crag.photo_count + crag.video_count"
+      >
+        {{ $t('components.crag.tabs.media') }}
+      </v-badge>
+    </v-tab>
+    <v-tab to="guide-books">
+      <v-badge
+        color="primary"
+        inline
+        :content="crag.guide_books.paper_count + crag.guide_books.web_count + crag.guide_books.pdf_count"
+      >
+        {{ $t('components.crag.tabs.guideBooks') }}
+      </v-badge>
+    </v-tab>
+    <v-tab to="maps">
+      {{ $t('components.crag.tabs.map') }}
+    </v-tab>
+  </v-tabs>
+</template>
+
+<script>
+export default {
+  name: 'CragTabs',
+  props: {
+    crag: Object
+  }
+}
+</script>
--- src/views/CragView.vue
+++ src/views/CragView.vue
@@ -2,44 +2,7 @@
   <div>
     <div v-if="!loadingCrag">
       <crag-head :crag="crag" />
-      <v-tabs
-        show-arrows
-      >
-        <v-tab to="infos">
-          Info
-        </v-tab>
-        <v-tab to="routes">
-          <v-badge
-            color="primary"
-            inline
-            :content="crag.routes_figures.route_count"
-          >
-            Voies
-          </v-badge>
-        </v-tab>
-        <v-tab to="photos">
-          <v-badge
-            color="primary"
-            inline
-            :content="crag.photo_count + crag.video_count"
-          >
-            Photos & Vidéos
-          </v-badge>
-        </v-tab>
-        <v-tab to="guide-books">
-          <v-badge
-            color="primary"
-            inline
-            :content="crag.guide_books.paper_count + crag.guide_books.web_count + crag.guide_books.pdf_count"
-          >
-            Topos
-          </v-badge>
-        </v-tab>
-        <v-tab to="maps">
-          Carte
-        </v-tab>
-      </v-tabs>
-
+      <crag-tabs :crag="crag" />
       <router-view></router-view>
     </div>
     <spinner v-if="loadingCrag" />
@@ -49,15 +12,12 @@
 import { CragConcern } from '@/concerns/CragConcern'
 import CragHead from '@/components/crags/layouts/CragHead'
 import Spinner from '@/components/layouts/Spiner'
+import CragTabs from '@/components/crags/layouts/CragTabs'
 
 export default {
   name: 'CragView',
-  components: { Spinner, CragHead },
+  components: { CragTabs, Spinner, CragHead },
   mixins: [CragConcern],
-  props: {
-    cragId: null,
-    slug: null
-  },
   watch: {
     '$route.params.cragId': 'getCrag'
   }
