e860a121656442d63f945441a4a9145b4e7e50e7
clucien <chastanlucien@gmail.com>
2021-01-21 15:12:54 +0100

    Amélioration tabs falaises

--- src/components/crags/layouts/CragTabs.vue
+++ src/components/crags/layouts/CragTabs.vue
@@ -9,28 +9,40 @@
       <v-badge
         color="primary"
         inline
+        v-if="crag.photo_count + crag.video_count"
         :content="crag.photo_count + crag.video_count"
       >
         {{ $t('components.crag.tabs.media') }}
       </v-badge>
+      <span v-else>
+        {{ $t('components.crag.tabs.media') }}
+      </span>
     </v-tab>
     <v-tab to="links">
       <v-badge
+        v-if="crag.link_count"
         color="primary"
         inline
         :content="crag.link_count"
       >
         {{ $t('components.crag.tabs.links') }}
       </v-badge>
+      <span v-else>
+        {{ $t('components.crag.tabs.links') }}
+      </span>
     </v-tab>
     <v-tab to="guide-books">
       <v-badge
         color="primary"
         inline
+        v-if="crag.guide_books.paper_count + crag.guide_books.web_count + crag.guide_books.pdf_count"
         :content="crag.guide_books.paper_count + crag.guide_books.web_count + crag.guide_books.pdf_count"
       >
         {{ $t('components.crag.tabs.guideBooks') }}
       </v-badge>
+      <span v-else>
+        {{ $t('components.crag.tabs.guideBooks') }}
+      </span>
     </v-tab>
     <v-tab to="maps">
       {{ $t('components.crag.tabs.map') }}
