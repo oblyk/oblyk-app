983e631a04e21008279fcf44b38d3b4571a1e105
clucien <chastanlucien@gmail.com>
2021-02-11 12:36:47 +0100

    Traduction boîte carnet de croix

--- src/components/homes/HomeBoxAscentsLog.vue
+++ src/components/homes/HomeBoxAscentsLog.vue
@@ -2,12 +2,9 @@
   <v-row>
     <v-col align-self="center" cols="md-6" order-md="last">
       <h2 class="loved-by-king">
-        Tiens un carnet de croix
+        {{ $t('home.ascents.title') }}
       </h2>
-      <p>
-        Bon pour la mémoire, la progression et la motivation, tenir un carnet de croix à beaucoup d'avantages.
-        Oblyk te permet de le faire facilement, tu pourras ensuite visualiser ta progression grâce à de nombreuses façons d'analyser et d'afficher ton carnet de croix.
-      </p>
+      <p v-html="$t('home.ascents.body')" />
     </v-col>
     <v-col align-self="center" cols="md-6">
       <v-img :src="require('@/assets/svgs/homes/ascents-log.svg')" />
