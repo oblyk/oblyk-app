a5136a5743621eae885dede28a99f7bff424e35c
clucien <chastanlucien@gmail.com>
2021-02-15 11:28:20 +0100

    Traduction de la home box oblyk et les topos

--- src/components/homes/HomeBoxGuideBook.vue
+++ src/components/homes/HomeBoxGuideBook.vue
@@ -2,16 +2,10 @@
   <v-row>
     <v-col align-self="center" cols="md-6">
       <h2 class="loved-by-king">
-        Oblyk et les topos
+        {{ $t('home.oblykAndGuide.title') }}
       </h2>
-      <p>
-        Vous pourriez vous demander si Oblyk peut remplacer les topos papier et par extension nuire au développement des sites d'escalade ?
-        Sachez que nous nous sommes beaucoup posé cette question aussi. Et la réponse est non, Oblyk ne peut pas remplacer un topo !
-        Les topos contiennent des informations indispensables que nous ne permettons pas d'ajouter sur oblyk
-      </p>
-      <p>
-        Acheter un topo, c'est soutenir des passionnés qui donnent énormément pour maintenir, développer, nettoyer les sites d'escalade sur lesquels vous allez grimper gratuitement.
-      </p>
+      <p v-html="$t('home.oblykAndGuide.body1')" />
+      <p v-html="$t('home.oblykAndGuide.body2')" />
     </v-col>
     <v-col align-self="center" cols="md-6">
       <v-img :src="require('@/assets/svgs/homes/guide-book.svg')" />
