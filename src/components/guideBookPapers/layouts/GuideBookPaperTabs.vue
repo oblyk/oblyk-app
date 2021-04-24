d61bc288116838f54b32b56f20f41e21366c4c4b
clucien <chastanlucien@gmail.com>
2021-04-24 23:16:49 +0200

    On cache les badge s'il n'y pas d'élément à lister sur les topos

--- src/components/guideBookPapers/layouts/GuideBookPaperTabs.vue
+++ src/components/guideBookPapers/layouts/GuideBookPaperTabs.vue
@@ -2,36 +2,58 @@
   <v-tabs
     show-arrows
   >
+
+    <!-- Info tabs -->
     <v-tab to="infos">
       {{ $t('components.guideBookPaper.tabs.info') }}
     </v-tab>
+
+    <!-- Crags list tabs -->
     <v-tab to="crags">
       <v-badge
         color="primary"
         inline
+        v-if="guideBookPaper.crags_count > 0"
         :content="guideBookPaper.crags_count"
       >
         {{ $t('components.guideBookPaper.tabs.crags') }}
       </v-badge>
+      <span v-else>
+        {{ $t('components.guideBookPaper.tabs.crags') }}
+      </span>
     </v-tab>
+
+    <!-- Photos tabs -->
     <v-tab to="photos">
       <v-badge
         color="primary"
         inline
+        v-if="guideBookPaper.photos_count > 0"
         :content="guideBookPaper.photos_count"
       >
         {{ $t('components.guideBookPaper.tabs.photos') }}
       </v-badge>
+      <span v-else>
+        {{ $t('components.guideBookPaper.tabs.photos') }}
+      </span>
     </v-tab>
+
+    <!-- Links tabs -->
     <v-tab to="links">
       <v-badge
         color="primary"
         inline
         :content="guideBookPaper.links_count"
+        v-if="guideBookPaper.links_count > 0"
       >
         {{ $t('components.guideBookPaper.tabs.links') }}
       </v-badge>
+      <span v-else>
+        {{ $t('components.guideBookPaper.tabs.links') }}
+      </span>
     </v-tab>
+
+    <!-- Map tabs -->
     <v-tab to="map">
       {{ $t('components.guideBookPaper.tabs.map') }}
     </v-tab>
