bc544929df4e0ace7a1f941faf8a5975ba95f694
clucien <chastanlucien@gmail.com>
2021-05-01 20:29:28 +0200

    Réorganisation du menu des cartes

--- src/components/layouts/AppDrawer.vue
+++ src/components/layouts/AppDrawer.vue
@@ -105,12 +105,6 @@
           </v-list-item-content>
         </template>
         <app-drawer-item
-          v-if="isLoggedIn"
-          url="/maps/my-map"
-          icon="mdi-map-check"
-          :title="$t('components.layout.appDrawer.myMap')"
-        />
-        <app-drawer-item
           url="/maps/crags"
           icon="mdi-terrain"
           :title="$t('components.layout.appDrawer.mapCrags')"
@@ -124,6 +118,13 @@
           url="/maps/climbers"
           icon="mdi-account-group"
           :title="$t('components.layout.appDrawer.mapClimber')"
+        />
+        <v-divider v-if="isLoggedIn" inset />
+        <app-drawer-item
+          v-if="isLoggedIn"
+          url="/maps/my-map"
+          icon="mdi-map-check"
+          :title="$t('components.layout.appDrawer.myMap')"
         />
       </v-list-group>
 
