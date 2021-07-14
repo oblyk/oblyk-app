994ac72bdd23591abfda699dd248172266897c5e
clucien <chastanlucien@gmail.com>
2021-07-14 15:49:58 +0200

    On partage le dossier node_modules pour gagner de la place et du temps d'installation

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,13 @@ lock "~> 3.14.1"
 set :application, "oblyk-app"
 set :repo_url, "git@github.com:lucien-chastan/oblyk-app.git"
 set :linked_files, fetch(:linked_files, []).push('.env', '.env.local')
+set :linked_dirs, fetch(:linked_dirs, []).push('node_modules')
 set :npm_method, 'ci'
 set :keep_releases, 5
 
 after 'deploy:updated', 'app:build'
 after 'deploy:publishing', 'pm2:restart'
-
+before 'deploy:reverted', 'npm:install'
 
 namespace :pm2 do
   desc 'Start or restart pm2'
