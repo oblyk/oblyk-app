a46ba05ac6295e63406dea939eeffe3ac4d89fe1
clucien <chastanlucien@gmail.com>
2021-10-30 14:05:11 +0200

    Paramétrage de moment pour importer uniquement les locals qui nous intérésse

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const webpack = require('webpack')
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  configureWebpack: {
+    plugins: [
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/locale$/,
+        contextRegExp: /moment$/
+      })
+    ]
+  }
 }
