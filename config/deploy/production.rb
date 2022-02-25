8bcd2025794e76abebb17b40d34dbb8190456e86
clucien <chastanlucien@gmail.com>
2022-02-25 20:02:45 +0100

    Changement du domaine de déploiement

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-server 'next.oblyk.org', user: 'lucien', roles: %w[app], port: 1622
+server 'oblyk.org', user: 'lucien', roles: %w[app], port: 1622
 
 set :deploy_to, '/var/www/oblyk/app'
 
