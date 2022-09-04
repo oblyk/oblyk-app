3a7fabb04624b17b5cb398badaa63b520f86999c
clucien <chastanlucien@gmail.com>
2022-09-04 15:32:30 +0200

    On affiche la modal du daily ascents en fullscreen sur mobile On ajoute un query au niveau de la route à l'ouverture de la modal pour intercepter le bouton retour

--- components/logBooks/outdoors/DailyAscentsPart.vue
+++ components/logBooks/outdoors/DailyAscentsPart.vue
@@ -1,65 +1,75 @@
 <template>
   <div class="daily-ascent-crag">
+    <v-img
+      :src="Crag.coverUrl"
+      :lazy-src="Crag.thumbnailCoverUrl"
+      class="hoverable"
+      width="250"
+      height="160"
+      dark
+      gradient="to bottom, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%"
+      @click="openDialogue()"
+    >
+      <div class="daily-ascent-crag-description">
+        <p class="mb-n2">
+          {{ Crag.name }}
+        </p>
+        <p class="mb-0">
+          <small class="font-italic">
+            {{ part.ascents.length }} {{ $t('common.crosses') }} {{ dateFromNow(part.date) }}
+          </small>
+        </p>
+      </div>
+    </v-img>
+
     <v-dialog
       v-model="dialog"
+      :fullscreen="isMobile"
       width="500"
     >
-      <template #activator="{ on, attrs }">
+      <v-card
+        class="d-flex flex-column justify-space-between"
+        :class="isMobile ? 'rounded-0' : ''"
+      >
         <v-img
+          class="flex-grow-0"
+          height="160"
           :src="Crag.coverUrl"
           :lazy-src="Crag.thumbnailCoverUrl"
-          class="hoverable"
-          width="250"
-          height="160"
-          dark
-          gradient="to bottom, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%"
-          v-bind="attrs"
-          v-on="on"
-        >
-          <div class="daily-ascent-crag-description">
-            <p class="mb-n2">
+        />
+        <div class="flex-grow-1 d-flex flex-column justify-space-between">
+          <div class="flex-grow-0">
+            <v-card-title>
               {{ Crag.name }}
-            </p>
-            <p class="mb-0">
-              <small class="font-italic">
-                {{ part.ascents.length }} {{ $t('common.crosses') }} {{ dateFromNow(part.date) }}
-              </small>
-            </p>
+            </v-card-title>
+            <v-card-subtitle
+              v-html="$t('components.dailyCrosses.iMade', { count: part.ascents.length, ago: dateFromNow(part.date), date: humanizeDate(part.date) })"
+            />
           </div>
-        </v-img>
-      </template>
 
-      <v-card>
-        <v-img
-          height="160"
-          :src="Crag.coverUrl"
-          :lazy-src="Crag.thumbnailCoverUrl"
-        />
-        <v-card-title>
-          {{ Crag.name }}
-        </v-card-title>
-        <v-card-subtitle
-          v-html="$t('components.dailyCrosses.iMade', { count: part.ascents.length, ago: dateFromNow(part.date), date: humanizeDate(part.date) })"
-        />
-
-        <v-card-text>
-          <crag-route-small-line
-            v-for="(ascent, routeIndex) in part.ascents"
-            :key="`route-index-${routeIndex}`"
-            :route="cragRouteToObject(ascent.crag_route)"
-          />
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
+          <v-card-text
+            class="flex-grow-1 overflow-y-auto"
           >
-            {{ $t('actions.close') }}
-          </v-btn>
-        </v-card-actions>
+            <crag-route-small-line
+              v-for="(ascent, routeIndex) in part.ascents"
+              :key="`route-index-${routeIndex}`"
+              :route="cragRouteToObject(ascent.crag_route)"
+            />
+          </v-card-text>
+
+          <v-card-actions
+            class="flex-grow-0"
+          >
+            <v-spacer />
+            <v-btn
+              color="primary"
+              text
+              @click="closeDialogue()"
+            >
+              {{ $t('actions.close') }}
+            </v-btn>
+          </v-card-actions>
+        </div>
       </v-card>
     </v-dialog>
   </div>
@@ -85,6 +95,7 @@ export default {
 
   data () {
     return {
+      isMobile: false,
       dialog: false
     }
   },
@@ -95,9 +106,42 @@ export default {
     }
   },
 
+  watch: {
+    $route (newVal, oldVal) {
+      if (oldVal.query?.dialog === 'true') {
+        this.dialog = false
+      }
+    }
+  },
+
+  mounted () {
+    this.isMobile = window.innerWidth < 600
+  },
+
   methods: {
     cragRouteToObject (route) {
       return new CragRoute({ attributes: route })
+    },
+
+    openDialogue () {
+      this.dialog = true
+      this.$router.push(
+        {
+          path: this.$route.fullPath,
+          query: {
+            dialog: true
+          }
+        }
+      )
+    },
+
+    closeDialogue () {
+      this.dialog = false
+      this.$router.push(
+        {
+          path: this.$route.path
+        }
+      )
     }
   }
 }
