71fbbc2caa0a6e4fa845a631654c05f2669f34b5
clucien <chastanlucien@gmail.com>
2022-10-04 21:33:33 +0200

    Correction dans le concerne de protection des salles administrées

--- concerns/ProtectedGymConcern.js
+++ concerns/ProtectedGymConcern.js
@@ -9,9 +9,14 @@ export const ProtectedGymConcern = {
     returnIfAdministered () {
       // return if gym is not administered
       if (!this.gym.administered) { return }
+      if (!this.$auth.loggedIn) { return }
 
       // if current user administre gym but the right one
-      if (!this.administeredGyms.includes(this.gym.id)) {
+      const gymIds = []
+      for (const gym of this.$auth.user.administered_gyms) {
+        gymIds.push(gym.id)
+      }
+      if (!gymIds.includes(this.gym.id)) {
         this.$router.push(`${this.gym.path}/administrator-required`)
       }
     }
