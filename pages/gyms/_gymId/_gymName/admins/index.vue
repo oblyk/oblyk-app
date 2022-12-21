31e3a34287243145a49879196e0554e36a50faba
clucien <chastanlucien@gmail.com>
2022-12-21 23:12:46 +0100

    Ajout d'un effet visuel de loading pendant le chargement de la zone d'admin d'une salle

--- pages/gyms/_gymId/_gymName/admins/index.vue
+++ pages/gyms/_gymId/_gymName/admins/index.vue
@@ -1,54 +1,79 @@
 <template>
-  <v-container v-if="gym" fluid>
-    <v-breadcrumbs
-      :items="breadcrumbs"
-      class="mb-1"
-    />
-    <v-row>
-      <v-col class="col-md-12 pa-2">
-        <gym-admin-welcome :gym="gym" />
-      </v-col>
-    </v-row>
+  <v-container fluid>
+    <!-- Gym admin loader -->
+    <div v-if="!gym">
+      <v-breadcrumbs
+        :items="[{ text: '...' }, { text: '...' }]"
+        class="mb-1"
+      />
+      <v-row>
+        <v-col cols="12">
+          <v-skeleton-loader type="article" />
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-skeleton-loader type="card-heading" />
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-skeleton-loader type="card-heading" />
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-skeleton-loader type="card-heading" />
+        </v-col>
+      </v-row>
+    </div>
 
-    <!-- Guide book settings -->
-    <v-row>
-      <div class="col-sm-12 mt-10">
-        <p class="subtitle-1 mb-0">
-          <v-icon left>
-            {{ mdiMapLegend }}
-          </v-icon>
-          {{ $t('components.gymAdmin.GuideBookSettings') }}
-        </p>
-      </div>
-    </v-row>
-    <v-row>
-      <div class="col-sm-12 col-md-6 col-lg-4 pa-2">
-        <gym-admin-route-figures :gym="gym" />
-      </div>
-      <div class="col-sm-12 col-md-6 col-lg-4 pa-2">
-        <gym-admin-space-figures :gym="gym" />
-      </div>
-      <div class="col-sm-12 col-md-6 col-lg-4 pa-2">
-        <gym-admin-difficulty-system-figures :gym="gym" />
-      </div>
-    </v-row>
+    <!-- Gym admin -->
+    <div v-if="gym">
+      <v-breadcrumbs
+        :items="breadcrumbs"
+        class="mb-1"
+      />
+      <v-row>
+        <v-col class="col-md-12 pa-2">
+          <gym-admin-welcome :gym="gym" />
+        </v-col>
+      </v-row>
 
-    <!-- Gestion settings -->
-    <v-row>
-      <div class="col-sm-12 mt-10">
-        <p class="subtitle-1 mb-0">
-          <v-icon left>
-            {{ mdiTune }}
-          </v-icon>
-          {{ $t('components.gymAdmin.gestionSettings') }}
-        </p>
-      </div>
-    </v-row>
-    <v-row>
-      <div class="col-sm-12 col-md-6 col-lg-4 pa-2">
-        <gym-admin-team-figures :gym="gym" />
-      </div>
-    </v-row>
+      <!-- Guide book settings -->
+      <v-row>
+        <div class="col-sm-12 mt-10">
+          <p class="subtitle-1 mb-0">
+            <v-icon left>
+              {{ mdiMapLegend }}
+            </v-icon>
+            {{ $t('components.gymAdmin.GuideBookSettings') }}
+          </p>
+        </div>
+      </v-row>
+      <v-row>
+        <div class="col-sm-12 col-md-6 col-lg-4 pa-2">
+          <gym-admin-route-figures :gym="gym" />
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 pa-2">
+          <gym-admin-space-figures :gym="gym" />
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 pa-2">
+          <gym-admin-difficulty-system-figures :gym="gym" />
+        </div>
+      </v-row>
+
+      <!-- Gestion settings -->
+      <v-row>
+        <div class="col-sm-12 mt-10">
+          <p class="subtitle-1 mb-0">
+            <v-icon left>
+              {{ mdiTune }}
+            </v-icon>
+            {{ $t('components.gymAdmin.gestionSettings') }}
+          </p>
+        </div>
+      </v-row>
+      <v-row>
+        <div class="col-sm-12 col-md-6 col-lg-4 pa-2">
+          <gym-admin-team-figures :gym="gym" />
+        </div>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
