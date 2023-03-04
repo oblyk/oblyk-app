7c3910b4890c38fcbd036448e48d4b9ddeb87aa0
clucien <chastanlucien@gmail.com>
2023-03-04 14:43:03 +0100

    Correction parcours des droits s'ils sont null

--- mixins/GymRolesHelpers.js
+++ mixins/GymRolesHelpers.js
@@ -2,7 +2,7 @@ export const GymRolesHelpers = {
   methods: {
     gymAuthCan (gym, action) {
       const administeredGym = this.$auth.user.gym_roles.find(administeredGym => administeredGym.gym_id === gym.id)
-      for (const role of administeredGym.roles) {
+      for (const role of (administeredGym.roles || [])) {
         if (role === action) {
           return true
         }
