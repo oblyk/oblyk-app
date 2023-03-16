ed9205a2701a9fc1f828501a7d7dafbe9f9b7d49
clucien <chastanlucien@gmail.com>
2023-03-16 20:28:25 +0100

    Ajout des dossiers pour la mise en place du fichier assetlinks.json

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.16.0"
 
 set :application, "oblyk"
 set :repo_url, "git@github.com:oblyk/oblyk-app.git"
-set :linked_files, fetch(:linked_files, []).push('.env', 'ecosystem.config.js')
+set :linked_files, fetch(:linked_files, []).push('.env', 'ecosystem.config.js', 'static/.well-known/assetlinks.json')
 set :linked_dirs, fetch(:linked_dirs, []).push('node_modules', 'log')
 set :npm_method, 'ci'
 set :pm2_role, 'app'
