23df45811d891ea6aacecad5cfc9072be204b749
clucien <chastanlucien@gmail.com>
2026-03-21 12:23:16 +0100

    Correction affichage de la description d'un Topo PDF trop long

--- components/guideBookPdfs/GuideBookPdfListItem.vue
+++ components/guideBookPdfs/GuideBookPdfListItem.vue
@@ -19,11 +19,21 @@
       <v-list-item-title class="font-weight-bold">
         {{ guideBookPdf.name }}
       </v-list-item-title>
-      <v-list-item-subtitle>
+      <v-list-item-subtitle class="font-weight-medium mb-1">
         {{ infos() }}
       </v-list-item-subtitle>
-      <v-list-item-subtitle class="text-wrap">
-        {{ guideBookPdf.description }}
+      <v-list-item-subtitle
+        v-if="description"
+        class="text-wrap"
+        @click="onDescriptionClick"
+      >
+        {{ description }}
+        <small
+          v-if="!showMore && descriptionIsTooLong"
+          class="font-weight-medium hoverable blue--text text--darken-1"
+        >
+          {{ $t('actions.readMore') }}
+        </small>
       </v-list-item-subtitle>
     </v-list-item-content>
   </v-list-item>
@@ -43,12 +53,43 @@ export default {
     }
   },
 
+  data () {
+    return {
+      showMore: false
+    }
+  },
+
+  computed: {
+    descriptionIsTooLong () {
+      return this.guideBookPdf.description.length > 80
+    },
+
+    description () {
+      let description = this.guideBookPdf.description
+      if (this.descriptionIsTooLong && !this.showMore) {
+        description = this.guideBookPdf.description.substring(0, 80).split(' ')
+        description.pop()
+        description = description.join(' ')
+        description = `${description}...`
+      }
+      return description
+    }
+  },
+
   methods: {
     infos () {
       const infos = []
       if (this.guideBookPdf.author) { infos.push(this.guideBookPdf.author) }
       if (this.guideBookPdf.publication_year) { infos.push(this.guideBookPdf.publication_year) }
       return infos.join(', ')
+    },
+
+    onDescriptionClick (event) {
+      if (this.descriptionIsTooLong && this.showMore === false) {
+        event.preventDefault()
+        this.showMore = true
+        return false
+      }
     }
   }
 }
